0db10f84c8af5235cdc98d0b56384d16c1564969
Manuel Domínguez <mftienda@gmail.com>
2019-08-03 19:57:37 +0200

    1.-Gestion de usuarios

---
+++ Sección 1 - Gestión de usuarios y grupos/Crear-Usuario.ps1
@@ -0,0 +1,5 @@
+﻿Clear
+$usuario=Read-Host "Introduce nombre de usuario"
+$contra=Read-Host "Introduce contraseña" -Assecurestring
+New-LocalUser $usuario -Password $contra
+Add-LocalGroupMember usuarios -Member $usuario
---
+++ Sección 1 - Gestión de usuarios y grupos/Eliminacion-Usuarios.ps1
@@ -0,0 +1,6 @@
+﻿#Eliminación de usuarios de forma masiva
+$usuarios = Import-Csv -Path D:\usuarios.csv
+foreach ($i in $usuarios)
+        {
+        Remove-LocalUser $i.nombre
+        }
