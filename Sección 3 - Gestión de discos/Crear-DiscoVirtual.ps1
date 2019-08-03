b1fde62d1850599081dd73820628f703dbadbb4f
Manuel Domínguez <mftienda@gmail.com>
2019-08-03 19:59:55 +0200

    3.-Gestion de discos

---
+++ Sección 3 - Gestión de discos/Crear-DiscoVirtual.ps1
@@ -0,0 +1,9 @@
+﻿#Creación automática de un disco virtual
+$Disco=Read-Host "Introduce el nombre del disco (Path)"
+[int]$temp = Read-Host “Tamaño del disco en GBytes”
+[double]$tamano = $temp * 1000000000
+New-VHD -Path $Disco -SizeBytes $tamano –Dynamic
+Mount-VHD -Path $Disco -Passthru|
+Initialize-Disk -PassThru|
+New-Partition -AssignDriveLetter -UseMaximumSize|
+Format-Volume -FileSystem NTFS -Confirm:$false
